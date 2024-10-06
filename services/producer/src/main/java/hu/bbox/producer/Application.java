826847807debd24402fe9e6aea5a26cdc5e30d98
Csaba Szabó <szabcsabest@gmail.com>
2024-10-06 12:53:40 +0200

    init commit

---
+++ services/producer/src/main/java/hu/bbox/producer/Application.java
@@ -0,0 +1,13 @@
+package hu.bbox.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(hu.bbox.producer.Application.class, args);
+	}
+
+}
