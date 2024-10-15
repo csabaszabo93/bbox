e7564722a97630aa95ea1b60282e2450f85ac5ef
Csaba Szabó <szabcsabest@gmail.com>
2024-10-15 23:17:43 +0200

    adding ssl

--- services/producer/src/main/java/hu/bbox/producer/config/TcpClientConfig.java
+++ services/producer/src/main/java/hu/bbox/producer/config/TcpClientConfig.java
@@ -8,13 +8,19 @@ import hu.bbox.producer.handlers.LoggingRegistrationHandler;
 import hu.bbox.producer.handlers.SimpleSoapRequestHandler;
 import hu.bbox.producer.model.ResponseContainer;
 import hu.bbox.producer.repositories.RandomProductVatStore;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
 import reactor.core.publisher.Mono;
 import reactor.netty.NettyOutbound;
 import reactor.netty.tcp.TcpClient;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -54,10 +60,18 @@ public class TcpClientConfig {
     public TcpClient tcpClient(
             @Value("${hu.bbox.proxy.producer.port:45820}") int port,
             @Value("${hu.bbox.proxy.host:localhost}") String proxyHost,
+            @Value("${hu.bbox.proxy.ssl:true}") boolean useSsl,
             MessageSerializer<Envelope<String>> messageSerializer,
-            BiConsumer<Envelope<String>, NettyOutbound> envelopeHandler) {
-        return TcpClient
-                .create()
+            BiConsumer<Envelope<String>, NettyOutbound> envelopeHandler) throws IOException {
+        TcpClient tcpClient = TcpClient.create();
+        if (useSsl) {
+            ClassPathResource resource = new ClassPathResource("ssl/cert_auth.pem");
+            InputStream inputStream = new InputStreamResource(resource).getInputStream();
+            SslContext sslcontext = SslContextBuilder.forClient().trustManager(inputStream).build();
+            tcpClient.secure(spec -> spec.sslContext(sslcontext));
+        }
+
+        return tcpClient
                 .host(proxyHost)
                 .port(port)
                 .handle((in, out) -> {
--- services/proxy/src/main/java/hu/bbox/proxy/Application.java
+++ services/proxy/src/main/java/hu/bbox/proxy/Application.java
@@ -5,13 +5,20 @@ import hu.bbox.messaging.parser.KryoStringEnvelopeSerializer;
 import hu.bbox.proxy.handlers.ConnectionHandler;
 import hu.bbox.proxy.handlers.NettyConnectionHandler;
 import hu.bbox.proxy.message.AsyncMessageSender;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
 import reactor.netty.DisposableServer;
+import reactor.netty.tcp.SslProvider;
 import reactor.netty.tcp.TcpServer;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 import java.util.function.Supplier;
 
@@ -39,19 +46,27 @@ public class Application {
 		return new NettyConnectionHandler(idGenerator, new KryoStringEnvelopeSerializer(), proxy);
 	}
 
-	@Bean
+	@Bean(destroyMethod = "disposeNow")
 	public DisposableServer producerTcpServer(
-			@Value("${hu.bbox.proxy.producer.host:0.0.0.0}") String host,
+			@Value("${hu.bbox.proxy.host:0.0.0.0}") String host,
 			@Value("${hu.bbox.proxy.producer.port:45820}") int port,
-			ConnectionHandler connectionHandler) {
-		return TcpServer
-				.create()
+			@Value("${hu.bbox.proxy.ssl:true}") boolean useSsl,
+			ConnectionHandler connectionHandler) throws IOException {
+		TcpServer tcpServer = TcpServer.create();
+		if (useSsl) {
+			ClassPathResource certResource = new ClassPathResource("ssl/bbox.proxy.crt");
+			InputStream certStream = new InputStreamResource(certResource).getInputStream();
+			ClassPathResource keyResource = new ClassPathResource("ssl/bbox.proxy.key");
+			InputStream keyStream = new InputStreamResource(keyResource).getInputStream();
+			SslContext sslcontext = SslContextBuilder.forServer(certStream, keyStream).build();
+			tcpServer.secure(spec -> spec.sslContext(sslcontext));
+		}
+		return tcpServer
 				.host(host)
 				.port(port)
 				.childObserve(connectionHandler)
 				.doOnConnection(connectionHandler)
 				.handle(connectionHandler)
-				//.secure()
 				.bindNow();
 	}
 
